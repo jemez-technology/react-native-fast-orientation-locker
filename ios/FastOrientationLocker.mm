98868a1acd1003dbb30298a03ef5cdeeeb4532ca
teststation5 <joe@vidunas.net>
2025-11-28 12:34:36 -0700

    use objective-c port of swift file to avoid missing swift.h header

--- ios/FastOrientationLocker.mm
+++ ios/FastOrientationLocker.mm
@@ -1,27 +1,70 @@
 #import "FastOrientationLocker.h"
-#import "FastOrientationLocker-Swift.h"
 @implementation FastOrientationLocker {
-  FastOrientationLockerImpl *moduleImpl;
+    UIInterfaceOrientationMask currentOrientationMask;
 }
 -(instancetype) init {
   self = [super init];
-  if(self) {
-    moduleImpl = [FastOrientationLockerImpl new];
-  }
   return self;
 }
 RCT_EXPORT_MODULE()
 
+- (void) forceRotation:(UIInterfaceOrientation)orientation {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIInterfaceOrientationMask lockMask;
+        switch(orientation) {
+            case UIInterfaceOrientationPortrait:
+                lockMask = UIInterfaceOrientationMaskPortrait;
+                break;
+            case UIInterfaceOrientationLandscapeLeft:
+            case UIInterfaceOrientationLandscapeRight:
+                lockMask = UIInterfaceOrientationMaskLandscape;
+                break;
+            default:
+                lockMask = UIInterfaceOrientationMaskAll;
+
+        }
+        self->currentOrientationMask = lockMask;
+        
+        if (@available(iOS 16.0, *)) {
+            UIWindowScene *windowScene = nil;
+            for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive) {
+                    if ([scene isKindOfClass:[UIWindowScene class]]) {
+                        windowScene = (UIWindowScene *)scene;
+                        break; // Found the active foreground scene, no need to continue
+                    }
+                }
+            }
+            if (windowScene != nil){
+                UIWindowSceneGeometryPreferencesIOS *preferences = [[UIWindowSceneGeometryPreferencesIOS alloc] initWithInterfaceOrientations:lockMask];
+                [windowScene requestGeometryUpdateWithPreferences:preferences errorHandler:^(NSError * _Nonnull error) {
+                    NSLog(@"❌ Orientation update failed: %@", error.localizedDescription);
+                }];
+            }
+
+        } else {
+            [UIDevice.currentDevice setValue:[NSNumber numberWithInteger: orientation] forKey:@"orientation"];
+            [UIViewController attemptRotationToDeviceOrientation];
+
+        }
+
+    });
+}
+
 - (void)lockToLandscape {
-  return [moduleImpl lockToLandscape];
+    [self forceRotation:UIInterfaceOrientationLandscapeLeft];
 }
 
 - (void)lockToPortrait {
-  return [moduleImpl lockToPortrait];
+    [self forceRotation:UIInterfaceOrientationPortrait];
 }
 
 - (void)unlockAllOrientations {
-  return [moduleImpl unlockAllOrientations];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self->currentOrientationMask = UIInterfaceOrientationMaskAll;
+        [UIDevice.currentDevice setValue:[NSNumber numberWithInteger: UIInterfaceOrientationUnknown] forKey:@"orientation"];
+        [UIViewController attemptRotationToDeviceOrientation];
+    });
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
